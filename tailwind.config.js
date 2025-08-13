972bd545e7efccdbf344b7d5c13d0afaa92a06c4
Esteban <estebanjosejulian@gmail.com>
2025-08-12 23:27:26 -0600

    Creacion del login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./*.{html,js,ts}",
-    "./src/**/*.{html,js,ts}"
+    "./src/**/*.{html,js,ts}",
+    './src/**/*.html',
   ],
   theme: {
     extend: {},
